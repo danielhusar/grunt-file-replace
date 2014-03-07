02e616584907fb53528df2dffe33821e727fd694
Daniel Husar <dano.husar@gmail.com>
2014-03-07 15:15:49 +0000

    tests

--- test/fileReplace_test.js
+++ test/fileReplace_test.js
@@ -12,16 +12,19 @@ exports.fileReplace = {
     test.expect(2);
 
     var actual = grunt.file.read('./test/fixtures/123');
-
     var expected = grunt.file.read('./test/expected/local');
-    test.equal(actual, expected, 'Local file should be copied.');
+    var expected2 = grunt.file.read('./test/expected/remote');
 
-    expected = grunt.file.read('./test/expected/remote');
-    test.equal(actual, expected, 'Remote file should be copied.');
+    setTimeout(function(){
+      test.equal(actual, expected, 'Local file should be copied.');
+      test.equal(actual, expected2, 'Remote file should be copied.');
+      test.done();
+    }, 1000);
+
 
     //fs.unlinkSync('./test/expected/local');
     //fs.unlinkSync('./test/expected/remote');
 
-    test.done();
+
   }
 };
