6324f843f5c7c32335945cee953416dafb6079e7
Daniel Husar <dano.husar@gmail.com>
2014-03-07 14:30:00 +0000

    logic + tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,8 @@ module.exports = function (grunt) {
     // Configuration to be run (and then tested).
     fileReplace: {
       patterns: {
-        'test/fixtures/123': 'test/expected/123'
+        'test/expected/local': 'test/fixtures/123',
+        'test/expected/remote': 'https://raw.github.com/danielhusar/grunt-file-replace/master/test/fixtures/123'
       }
     },
 
